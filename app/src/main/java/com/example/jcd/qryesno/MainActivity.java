b11357981cf6ea40862f22918cfc2d8043100372
Aleksandr Melekhin <melekhin@alternativaplatform.com>
2017-07-28 18:27:14 +0500

    qq

---
+++ app/src/main/java/com/example/jcd/qryesno/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.jcd.qryesno;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import github.nisrulz.qreader.QRDataListener;
+import github.nisrulz.qreader.QREader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+
+    // QREader
+    private SurfaceView mySurfaceView;
+    private QREader qrEader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Setup SurfaceView
+        // -----------------
+        mySurfaceView = (SurfaceView) findViewById(R.id.camera_view);
+
+        // Init QREader
+        // ------------
+        qrEader = new QREader.Builder(this, mySurfaceView, new QRDataListener() {
+            @Override
+            public void onDetected(final String data) {
+                Log.d("QREader", "Value : " + data);
+                text.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        text.setText(data);
+                    }
+                });
+            }
+        }).facing(QREader.BACK_CAM)
+                .enableAutofocus(true)
+                .height(mySurfaceView.getHeight())
+                .width(mySurfaceView.getWidth())
+                .build();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Init and Start with SurfaceView
+        // -------------------------------
+        qrEader.initAndStart(mySurfaceView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Cleanup in onPause()
+        // --------------------
+        qrEader.releaseAndCleanup();
+    }
+}
---
+++ app/src/main/java/com/example/jcd/qryesno/MainApp.java
@@ -0,0 +1,16 @@
+package com.example.jcd.qryesno;
+
+import android.app.Application;
+
+/**
+ * Created by melekhin on 28.07.2017.
+ */
+
+public class MainApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
